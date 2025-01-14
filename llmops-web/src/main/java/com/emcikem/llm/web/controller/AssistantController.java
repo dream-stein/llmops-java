cbc1e49c2aa8305f71626abb16454279da8675a0
Emcikem <18968479985@163.com>
2025-01-14 23:07:02 +0800

    feat(.service): controller

---
+++ llmops-web/src/main/java/com/emcikem/llm/web/controller/AssistantController.java
@@ -0,0 +1,36 @@
+package com.emcikem.llm.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Create with Emcikem on 2024/11/20
+ *
+ * @author Emcikem
+ * @version 1.0.0
+ */
+@RestController
+class AssistantController {
+
+    Assistant assistant;
+    StreamingAssistant streamingAssistant;
+
+    AssistantController(Assistant assistant, StreamingAssistant streamingAssistant) {
+        this.assistant = assistant;
+        this.streamingAssistant = streamingAssistant;
+    }
+
+    @GetMapping("/assistant")
+    public String assistant(@RequestParam(value = "message", defaultValue = "What time is it now?") String message) {
+        return assistant.chat(message);
+    }
+
+    @GetMapping(value = "/streamingAssistant", produces = TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamingAssistant(
+            @RequestParam(value = "message", defaultValue = "Tell me an interesting story in 100 words") String message) {
+        return streamingAssistant.chat(message);
+    }
+}

84103c7cf933c33b92cbc4ffea57744f6f7afe3b
wx40217 <yps302@163.com>
2025-01-15 15:24:08 +0800

    init LangChain

---
+++ llmops-service/src/main/java/com/emcikem/llm/service/aiservice/StreamingAssistant.java
@@ -0,0 +1,12 @@
+package com.emcikem.llm.service.aiservice;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.spring.AiService;
+import reactor.core.publisher.Flux;
+
+@AiService
+interface StreamingAssistant {
+
+    @SystemMessage("You are a polite assistant")
+    Flux<String> chat(String userMessage);
+}
--- llmops-web/src/main/java/com/emcikem/llm/web/controller/AssistantController.java
+++ llmops-web/src/main/java/com/emcikem/llm/web/controller/AssistantController.java
@@ -1,41 +1,34 @@
 package com.emcikem.llm.web.controller;
 
+import dev.langchain4j.service.spring.AiService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
-import javax.annotation.Resource;
+import reactor.core.publisher.Flux;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
 
 /**
- * Create with Emcikem on 2024/11/20
- *
- * @author Emcikem
- * @version 1.0.0
+ * This is an example of using an {@link AiService}, a high-level LangChain4j API.
  */
 @RestController
 class AssistantController {
-//    Assistant assistant;
-//    StreamingAssistant streamingAssistant;
-//
-//    AssistantController(Assistant assistant, StreamingAssistant streamingAssistant) {
-//        this.assistant = assistant;
-//        this.streamingAssistant = streamingAssistant;
-//    }
-//
-//    @GetMapping("/assistant")
-//    public String assistant(@RequestParam(value = "message", defaultValue = "What time is it now?") String message) {
-//        return assistant.chat(message);
-//    }
-//
-//    @GetMapping(value = "/streamingAssistant", produces = TEXT_EVENT_STREAM_VALUE)
-//    public Flux<String> streamingAssistant(
-//            @RequestParam(value = "message", defaultValue = "Tell me an interesting story in 100 words") String message) {
-//        return streamingAssistant.chat(message);
-//    }
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+
+    Assistant assistant;
+    StreamingAssistant streamingAssistant;
+
+    AssistantController(Assistant assistant, StreamingAssistant streamingAssistant) {
+        this.assistant = assistant;
+        this.streamingAssistant = streamingAssistant;
     }
 
+    @GetMapping("/assistant")
+    public String assistant(@RequestParam(value = "message", defaultValue = "What time is it now?") String message) {
+        return assistant.chat(message);
+    }
+
+    @GetMapping(value = "/streamingAssistant", produces = TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamingAssistant(
+            @RequestParam(value = "message", defaultValue = "Tell me an interesting story in 100 words") String message) {
+        return streamingAssistant.chat(message);
+    }
 }

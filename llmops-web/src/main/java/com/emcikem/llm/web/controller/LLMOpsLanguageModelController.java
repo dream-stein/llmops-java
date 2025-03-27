5041d124b796e06ffa72b784084fa7869572cd86
emcikem <2287697454@qq.com>
2025-03-28 01:42:00 +0800

    feat(.service)：定义controller

--- llmops-web/src/main/java/com/emcikem/llm/web/controller/LLMOpsLanguageModelController.java
+++ llmops-web/src/main/java/com/emcikem/llm/web/controller/LLMOpsLanguageModelController.java
@@ -1,5 +1,8 @@
 package com.emcikem.llm.web.controller;
 
+import com.emcikem.llm.common.entity.ApiResponse;
+import com.google.protobuf.Api;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/language-models")
 public class LLMOpsLanguageModelController {
+
+    /**
+     * 获取所有语言模型列表信息
+     * @return
+     */
+    @GetMapping
+    public ApiResponse<Void> getLanguageModels() {
+        return ApiResponse.success(null);
+    }
+
+    /**
+     * 获取指定模型的详细信息
+     * @return
+     */
+    @GetMapping("/{provider_name}/{model_name}")
+    public ApiResponse<Void> getLanguageModel() {
+        return ApiResponse.success(null);
+    }
 }

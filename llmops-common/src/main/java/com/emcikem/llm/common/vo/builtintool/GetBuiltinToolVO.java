a591514fd0e6492230314437b453fb57dd93523a
emcikem <2287697454@qq.com>
2025-03-28 01:03:48 +0800

    feat(.service)：修改基本模型

---
+++ llmops-common/src/main/java/com/emcikem/llm/common/vo/builtintool/GetBuiltinToolVO.java
@@ -0,0 +1,30 @@
+package com.emcikem.llm.common.vo.builtintool;
+
+import com.emcikem.llm.common.vo.tools.ApiProviderToolVO;
+import com.emcikem.llm.common.vo.tools.ApiToolProvider;
+
+import java.util.List;
+
+/**
+ * Create with Emcikem on 2025/3/28
+ *
+ * @author Emcikem
+ * @version 1.0.0
+ */
+public class GetBuiltinToolVO {
+
+    private String background;
+
+    private String category;
+
+    private Long created_at;
+
+    private String description;
+
+    private String label;
+
+    private String name;
+
+    // todo:
+//    private List<> tools;
+}
